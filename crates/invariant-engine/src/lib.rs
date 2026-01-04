53a5b89d46713cd5b4aa6c245b8776b56e6fd52c
Jeancarlo Barrios <JeancarloBarrios@users.noreply.github.com>
2026-01-04 15:12:43 -0600

    feat: add engine base for invariant

--- crates/invariant-engine/src/lib.rs
+++ crates/invariant-engine/src/lib.rs
@@ -2,6 +2,6 @@ mod component_loader;
 mod engine;
 mod error;
 
-pub use component_loader::{C, ComponentSource};
+pub use component_loader::{ComponentLoader, ComponentSource};
 pub use engine::{EngineConfig, WasmEngine};
 pub use error::RuntimeError;
